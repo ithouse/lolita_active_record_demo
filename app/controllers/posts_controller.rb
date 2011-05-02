5e1dd8d89d3c923dd17b6405c4072247f61a048b
Gatis Tomsons <gatis@ithouse.lv>
2011-05-02 23:06:18 +0300

    added post index/show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,12 @@
 class PostsController < ApplicationController
+  layout "application"
+
   def show
+    @post = Post.find_by_id(params[:id])
   end
 
   def index
+    @posts = Post.find(:all)
   end
 
 end
