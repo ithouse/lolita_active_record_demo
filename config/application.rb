7ca3f62029f03225774c1fc7775e2aaea015fdfb
Krists Ozols <krists.ozols@gmail.com>
2011-05-28 21:59:53 +0300

    file for component hooks in lib

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module LolitaActiveRecordDemo
   class Application < Rails::Application
+    require 'component_hooks'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
