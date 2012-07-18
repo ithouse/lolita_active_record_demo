30df880638b6e6b1af745349d1883e7189cb21a0
mariscilitis <mar4a999@gmail.com>
2012-07-18 09:52:31 +0300

    slider

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @articles = Article.latest.published.limit(Article::SHOW_IN_HOME_PAGE)
+    @slider = Slider.first
   end
 end
---
+++ spec/models/article_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe "Article" do
+  it "should not be valid if title, content or publication date is empty" do
+    article = Fabricate.build(:article, :title => "", :content => "", :publication_date => "")
+    article.valid?.should_be_false
+  end
+end
